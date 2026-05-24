b0fc74e9198e656c7471175b3ef6b62e8a2d40a3
Bence Balogh <bence.balogh@datapao.com>
2026-05-24 20:56:05 +0200

    Refactor code structure for improved readability and maintainability

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,15 +13,33 @@ author = 'Bence Balogh'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "nbsphinx",
+    "sphinx_copybutton",
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
-
-
+exclude_patterns = ["_build", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+
+# -- Options for nbsphinx -------------------------------------------------
+# https://nbsphinx.readthedocs.io/en/0.9.8/
+
+nbsphinx_allow_errors = True
+nbsphinx_execute = 'never'
+
+# -- Options for copybutton -------------------------------------------------
+# https://sphinx-copybutton.readthedocs.io/en/latest/index.html
+
+# Strip input prompts from copied code
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+# By default, the copy button copies all the text in a code cell, including prompts. 
+# Setting this to False will make it copy only the code, without the prompts.
+copybutton_only_copy_prompt_lines = False
